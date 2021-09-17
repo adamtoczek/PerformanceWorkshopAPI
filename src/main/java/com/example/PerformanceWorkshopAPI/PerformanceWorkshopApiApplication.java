ec0e139a4be4d7f1b969ad43c1b6b0f67102e56c
Adam <a.a.toczek@gmail.com>
2021-09-17 22:09:07 +0200

    Initial commit

---
+++ src/main/java/com/example/PerformanceWorkshopAPI/PerformanceWorkshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PerformanceWorkshopAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceWorkshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceWorkshopApiApplication.class, args);
+	}
+
+}
