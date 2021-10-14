0820452dbf811de7f506d05d3b3c99c9589bae85
Adam <a.a.toczek@gmail.com>
2021-10-14 18:43:25 +0200

    add 1000 preloaded records

--- src/main/java/com/example/performanceWorkshopAPI/LoadDatabase.java
+++ src/main/java/com/example/performanceWorkshopAPI/LoadDatabase.java
@@ -7,6 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Configuration
 class LoadDatabase {
@@ -17,6 +23,16 @@ class LoadDatabase {
     CommandLineRunner initDatabase(EmployeeRepository repository) {
 
         return args -> {
+            ClassPathResource resource = new ClassPathResource("MOCK_DATA.csv");
+            InputStream inputStream = resource.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            while(reader.ready()) {
+                String[] line = reader.readLine().split(",");
+                log.info("Preloading " + repository.save(new Employee(line[0], line[1], line[2])));
+
+            }
+
             log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
             log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
         };
