2ef69fd79bf11bfeddb6f88c5be2a6fb359c4ac3
Adam <a.a.toczek@gmail.com>
2021-10-14 23:52:47 +0200

    add restassured unit tests

--- src/test/java/com/example/performanceWorkshopAPI/PerformanceWorkshopApiApplicationTests.java
+++ src/test/java/com/example/performanceWorkshopAPI/PerformanceWorkshopApiApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example.performanceWorkshopAPI;
 
+import com.example.performanceWorkshopAPI.xrfToken.XRFToken;
+import com.example.performanceWorkshopAPI.xrfToken.XRFTokenController;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PerformanceWorkshopApiApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private EmployeeController employeeController;
+	@Autowired
+	private XRFTokenController xrfController;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+		assertThat(employeeController).isNotNull();
+		assertThat(xrfController).isNotNull();
 	}
 
+	@Test
+	public void missingXRFHeaderShouldReturnBadRequest() throws Exception {
+		given().when().get("http://localhost:" + port + "/employees/1").then().statusCode(400).body("error", equalTo("Bad Request"));
+	}
+
+	@Test
+	public void wrongXRFHeaderShouldReturnAnuthorised() throws Exception {
+		String resp = given().header("XRF-token","12345").when().get("http://localhost:" + port + "/employees/1").then().statusCode(403).extract().response().getBody().asString();
+		assertThat(resp).isEqualTo("Unauthorised");
+	}
+	@Test
+	public void corectXRFHeaderShouldReturnEmployee() throws Exception {
+		given().header("XRF-token","1234").when().get("http://localhost:" + port + "/employees/1").then().statusCode(200);
+	}
 }
