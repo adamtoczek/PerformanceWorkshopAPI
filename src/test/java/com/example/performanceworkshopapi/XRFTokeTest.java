29622c6625c7beb51e62a142ecba17357b2476ac
Adam <a.a.toczek@gmail.com>
2021-10-15 17:09:47 +0200

    add tests

---
+++ src/test/java/com/example/performanceworkshopapi/XRFTokeTest.java
@@ -0,0 +1,18 @@
+package com.example.performanceworkshopapi;
+
+import com.example.performanceworkshopapi.xrfToken.XRFToken;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class XRFTokeTest {
+    @Test
+    public void tokensShouldBeEquals() {
+        XRFToken t1 = new XRFToken("123456");
+        XRFToken t2 = new XRFToken("123456");
+
+        assertEquals(t1, t2);
+    }
+
+
+}
